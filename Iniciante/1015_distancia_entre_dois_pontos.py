c68e983cbfb15b543c459db24e78f4a4c5476228
Jeferson Machado <jeferson_m94@hotmail.com>
2021-03-30 00:00:50 -0300

    sem o sqrt

--- Iniciante/1015_distancia_entre_dois_pontos.py
+++ Iniciante/1015_distancia_entre_dois_pontos.py
@@ -1,8 +1,6 @@
-from math import sqrt
-
 x1, y1 = list(map(float, input().split(' ')))
 x2, y2 = list(map(float, input().split(' ')))
 
-dist = sqrt((x2 - x1)**2 + (y2 - y1) **2)
+dist = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
 print(f'{dist:.4f}')
--- Iniciante/1036_formula_bhaskara.py
+++ Iniciante/1036_formula_bhaskara.py
@@ -1,11 +1,9 @@
-from math import sqrt
-
 A, B, C = list(map(float, input().split(' ')))
 
 if A == 0 or (B**2 - 4 * A * C) < 0:
     print('Impossivel calcular')
 else:
-    R1 = (-B + sqrt(B**2 - 4 * A * C)) / (2 * A)
-    R2 = (-B - sqrt(B**2 - 4 * A * C)) / (2 * A)
+    R1 = (-B + (B**2 - 4 * A * C)**0.5) / (2 * A)
+    R2 = (-B - (B**2 - 4 * A * C)**0.5) / (2 * A)
     print(f'R1 = {R1:.5f}')
     print(f'R2 = {R2:.5f}')
